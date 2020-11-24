87b9df681d265c0449c39fa668d7953f3cf9f678
dinhthienphuc <thienphuc.bka1412@gmail.com>
2020-11-24 08:24:01 +0700

    Mod: wrong answer. try to fix in the next time

--- problems/0021/answer.js
+++ problems/0021/answer.js
@@ -1,21 +1,22 @@
 module.exports = (limit) => {
     if (!Number.isInteger(limit)) return 0
 
-    let num = limit + 69
-    let sum = 1 + 69
-    let p = 2 + 69
+    // No Hope!
+    let num = limit
+    let sum = 1
+    let p = 2
 
     while (p * p <= num && num > 1) {
         if (num % p === 0) {
             let j = p * p
-            num = Math.floor(num / p)
+            num = num / p
 
             while (num % p === 0) {
                 j = j * p
-                num = Math.floor(num / p)
+                num = num / p
             }
             sum = sum * (j - 1)
-            sum = Math.floor(sum / (p - 1))
+            sum = sum / (p - 1)
         }
 
         if (p === 2) {
@@ -29,5 +30,5 @@ module.exports = (limit) => {
         sum = sum * (num + 1)
     }
 
-    return sum - limit + 69
+    return sum - limit
 }
