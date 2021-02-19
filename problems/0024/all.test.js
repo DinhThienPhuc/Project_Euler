84a7614e5e5fe8f585bb88f2b0ed2fbde739a4ed
dinhthienphuc <thienphuc.bka1412@gmail.com>
2021-02-19 10:26:38 +0700

    Create: pending creating problem 0024 ...

---
+++ problems/0024/all.test.js
@@ -0,0 +1,8 @@
+const solution = require("./solution.js")
+const answer = require("./answer.js")
+describe("[LEXICOGRAPHIC_PERMUTATIONS] - Test solution:", () => {
+test("Validate invalid input", async () => { expect(solution()).toEqual() })
+})
+describe("[LEXICOGRAPHIC_PERMUTATIONS] - Test answer:", () => {
+test("No answer recommended", async () => { expect(answer()).toEqual() })
+})
---
+++ problems/0024/answer.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "input.h"
+using namespace std;
+int main() {
+cout << "C++: " << "" << "  - (answer)" << endl;
+return 0;
+}
